fb85764971152699dcaaa3e1e66007fe40df52b2
Felipe Rocha <felipergomes19@gmail.com>
2025-03-12 10:20:34 -0300

    Adiciona a pasta 'Aula-03' junto com dois arquivos

---
+++ Aula-03/Variaveis.php
@@ -0,0 +1,15 @@
+<?php
+$texto = "Isso é um texto";
+$Numero1 = 4;
+$Numero2 = 5;
+$valor = null;
+
+echo $texto;
+echo "<br>";
+echo gettype($texto), "<br>";
+echo gettype($Numero1),"<br>";
+echo gettype($Numero2), "<br>";
+echo gettype($valor);
+
+
+?>
